3c7f72e7d56e50d9cd6cf6055816f24ab3253fd0
Yash Kavaiya <yash.kavaiya3@gmail.com>
2025-09-03 05:27:06 +0000

    Generated by Spark: Fix all reported errors.

--- src/components/ManualEvaluation.tsx
+++ src/components/ManualEvaluation.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
-import { Separator } from "@/components/ui/sep
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Input } from "@/components/ui/input";
 import { Checkbox } from "@/components/ui/checkbox";
